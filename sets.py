8e278b8cbb8140d1b3af3a3ba65059e438afa688
Casey Murphy <st01k@lvaas.net>
2017-09-15 07:35:40 -0400

    changed operations to only take 2 sets, reformatted output

--- sets.py
+++ sets.py
@@ -2,68 +2,48 @@ import random
 import string
 
 # Casey
-# intersection
-# union
-# both lists in ascending order
-# U = {x | x is a whole number 0-200 (inclusive)}
-# print NOT A (asc)
-# print A-B (asc)
 
-def genSets(k, pop, numSets):
+maxDomain = 0
+def genSets(k, pop):
+    global maxDomain
+    maxDomain = pop
     sets = []
+    numSets = 2
     for i in range(numSets):
         sets.append(random.sample(range(pop), k))
     return sets
 
-def complement(u, set):
-    uni = list(range(u))
+def complement(set):
+    uni = list(range(maxDomain + 1))
     for item in set:
-        if item in uni:
-            uni.remove(item)
+        if item in uni: uni.remove(item)
     return uni
 
 def difference(A, B):
     diff = []
     for item in A:
         if item not in B: diff.append(item)
-
     return diff
 
-
-def union(sets):
+def union(A, B):
     union = []
-    # for i in A: union.append(i)
-    # for i in B:
-    #     if i not in union: union.append(i)
-
-    for set in sets:
-        for item in set:
-            if item not in union: union.append(item)
-
+    for i in A: union.append(i)
+    for i in B:
+        if i not in union: union.append(i)
     union.sort()
     return union
 
-def intersection(sets):
+def intersection(A, B):
     intersection = []
-    # for i in A:
-    #     if i in B: intersection.append(i)
-
-    for set in sets:
-        for item in set:
-            for otherSet in sets:
-                if otherSet != set:
-                    if item in otherSet:
-                        if item not in intersection:
-                            intersection.append(item)
-
-
+    for i in A:
+        if i in B: intersection.append(i)
     intersection.sort()
     return intersection
 
 # prints greeting
-def greeting():
+def greeting(s):
     print()
-    print('Sets v1')
+    print(s)
     print('------------------------')
     print('(type help for commands)')
     print('------------------------')
@@ -85,26 +65,50 @@ def prompt():
         print()
 
 def dialog():
-    n = input('enter number of sets> ')
     k = input('enter cardinality> ')
     pop = input('enter max value> ')
     print()
-    return genSets(int(k), int(pop), int(n))
+    return genSets(int(k), int(pop))
 
 
 def printResult(sets):
     alpha = string.ascii_uppercase
     for i, st in enumerate(sets):
         iden = alpha[i]
-        print('Set  ' + iden + ': { ' + str(st).strip('[]') + ' }')
+        #print('Set  ' + iden + ': { ' + str(st).strip('[]') + ' }')
+        print(buildSet('Set' + iden, st))
         st.sort()
-        print('Sorted: { ' + str(st).strip('[]') + ' }')
+        print(buildSet('Sorted', st))
+        #print('Sorted: { ' + str(st).strip('[]') + ' }')
         print()
 
-    u = union(sets)
-    i = intersection(sets)
-    print('Union : { ' + str(u).strip('[]') + ' }')
-    print('Inter : { ' + str(i).strip('[]') + ' }')
+    A, B = sets[0], sets[1]
+    u = union(A, B)
+    i = intersection(A, B)
+    c = complement(A)
+    d = difference(A, B)
+    print(buildSet('Union', u))
+    print(buildSet('Isect', i))
+    print(buildSet('NOT A', c))
+    print(buildSet('A - B', d))
+    #print('Union : { ' + str(u).strip('[]') + ' }\n')
+    # print('Isect : { ' + str(i).strip('[]') + ' }\n')
+    # print('NOT A : { ' + str(c).strip('[]') + ' }\n')
+    # print('A - B : { ' + str(d).strip('[]') + ' }\n')
+
+def buildSet(label, set):
+    temp = label + ':\n'
+    for i, item in enumerate(set):
+        if i % 10 == 0 and i > 0: temp += '\n'
+        temp += '{:>5}'.format(str(item))
+    return temp
+
+# runs test statement
+def test():
+    card, limit = 5, 10
+    sets = genSets(card, limit)
+    printResult(sets)
+    return
 
 # prints help menu
 def help():
@@ -119,21 +123,7 @@ def help():
 # clears screen
 def clear(): print('\n' * 70)
 
-# runs test statement
-def test():
-    card, limit, sets = 5, 10, 2
-    sets = genSets(card, limit, sets)
-    printResult(sets)
-    A = sets[0]
-    B = sets[1]
-    comp = complement(limit, A)
-    diff = difference(A, B)
-    print('NOT A : { ' + str(comp).strip('[]') + ' }')
-    print('A - B : { ' + str(diff).strip('[]') + ' }')
-
-    return
-
 if __name__ == "__main__":
-    greeting()
+    greeting('Sets v1')
     prompt()
     print('Goodbye')
