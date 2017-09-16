dc233753bf54d867c82113b78e30912b4c9748bf
Casey Murphy <st01k@lvaas.net>
2017-09-16 03:18:59 -0400

    commented sets.py

--- sets.py
+++ sets.py
@@ -1,10 +1,21 @@
+# Casey Murphy
+# Group 5, Project 5
+# Discrete Mathematics
+# 15 Sep 17
+
 import random
 
-# Casey
-
+# global variables
 maxDomain = 0
+
+# generates 2 sets of random numbers
+# assigns global max inclusive domain value
+# k - number of items in the set
+# pop - upper limit for number generation
+# returns a 2 member array of sets
 def genSets(k, pop):
     global maxDomain
+    # add 1 for inclusion on upper limit
     maxDomain = pop + 1
     sets = []
     numSets = 2
@@ -12,34 +23,62 @@ def genSets(k, pop):
         sets.append(random.sample(range(maxDomain), k))
     return sets
 
-def complement(set):
-    uni = list(range(maxDomain + 1))
-    for item in set:
-        if item in uni: uni.remove(item)
-    return uni
+# determines the complement of a set using
+# the global max domain value to define
+# the universal set from which to compare
+# {x | x ∈ U and x ∉ A}
+# set - a set of random numbers
+# returns the complement (NOT A) as a set
+def complement(A):
+    # create universal set
+    U = list(range(maxDomain))
+    for element in A:
+        # remove elements of A from universal set
+        if element in U: U.remove(element)
+    return U
 
+# determines the difference between two sets
+# {x | x ∈ A and x ∉ B}
+# A - minuend set
+# B - subtrahend set
+# returns the difference of A - B as a set
 def difference(A, B):
     diff = []
-    for item in A:
-        if item not in B: diff.append(item)
+    for element in A:
+        # adds elements of A that are not in B
+        if element not in B: diff.append(element)
     return diff
 
+# determines the union of two sets
+# {x | x ∈ A or x ∈ B}
+# A - first set
+# B - second set
+# returns the union of A and B as a set
 def union(A, B):
     union = []
-    for i in A: union.append(i)
-    for i in B:
-        if i not in union: union.append(i)
+    # adds all elements of A into union set
+    for element in A: union.append(element)
+    for element in B:
+        # adds element of B if not already in union set
+        if element not in union: union.append(element)
     union.sort()
     return union
 
+# determines the intersection of two sets
+# {x | x ∈ A and x ∈ B}
+# A - first set
+# B - second set
+# returns the intersection of A and B as a set
 def intersection(A, B):
     intersection = []
-    for i in A:
-        if i in B: intersection.append(i)
+    for element in A:
+        # adds element if it is in both A and B
+        if element in B: intersection.append(element)
     intersection.sort()
     return intersection
 
-# prints greeting
+# prints dynamic greeting
+# s - greeting title
 def greeting(s):
     print()
     print(s)
@@ -48,33 +87,71 @@ def greeting(s):
     print('------------------------')
 
 # displays prompt and awaits command input
-# calls function by command
+# validates input and calls function by command
 def prompt():
     while(True):
         i = input('main> ')
         print()
         if i == 'quit' or i == 'q': break
         elif i == 'help': help()
-        elif i == 'cls': clear()
+        elif i == 'cls' : clear()
         elif i == 'test': test()
-        elif i == 'eval':
-            k = input('enter cardinality> ')
-            if not k.isdigit() or int(k) < 0:
-                print('\ninvalid input\n')
-                continue
-            pop = input('enter max inclusive domain value> ')
-            print('domain: (0 - '+ pop +')')
-            if not pop.isdigit() or int(pop) < int(k):
-                print('\ninvalid input\n')
-                continue
-            sets = genSets(int(k), int(pop))
-            print()
-            printResult(sets)
+        elif i == 'eval': eval()
         else: print('invalid command')
         print()
 
+# runs test statement based on the
+# requirements for this project:
+# cardinality of 100
+# domain of (0 - 200)
+# generates sets
+# calls printResult to display results
+def test():
+    card, limit = 100, 200
+    sets = genSets(card, limit)
+    printResult(sets)
+    return
+
+# prompts for and validates user input
+# generates sets based on user input
+# calls printResult to display results
+def eval():
+    k = input('enter cardinality> ')
+    # ensure cardinality is greater than 0
+    if not k.isdigit() or int(k) < 0:
+        print('\ninvalid input\n')
+        return
+    pop = input('enter max inclusive domain value> ')
+    print('domain: (0 - '+ pop +')')
+    # ensure max domain is less than cardinality
+    # add one for inclusion
+    if not pop.isdigit() or int(pop) + 1 < int(k):
+        print('\ninvalid input\n')
+        return
+    sets = genSets(int(k), int(pop))
+    print()
+    printResult(sets)
+
+# prints help menu
+def help():
+    print('Available Commands')
+    print('-------------------------------------------')
+    print('help - prints this list')
+    print('cls  - clears console screen')
+    print('test - test on assignment requirements')
+    print('eval - prompts for custom set definition')
+    print('       ex: 5 and 10 for an easy-to-read test')
+    print('quit - exits program')
+
+# clears screen
+def clear(): print('\n' * 70)
+
+# prints results of operations on defined sets
+# calls operation functions and formatSet
+# sets - array of 2 sets
 def printResult(sets):
     alpha = ['A', 'B']
+    # prints set and sorted set for A and B
     for i, st in enumerate(sets):
         iden = alpha[i]
         print(formatSet('Set ' + iden, st))
@@ -88,38 +165,26 @@ def printResult(sets):
     print(formatSet('NOT A', complement(A)))
     print(formatSet('A - B', difference(A, B)))
 
+# formats a printable, readable string
+# label - name of set
+# set - set to format
+# returns a string containing label and set
 def formatSet(label, set):
     temp = label + ':\n'
     for i, item in enumerate(set):
+        # add new line every 10 elements
         if i % 10 == 0: temp += '\n'
+        # align elements with padding
         temp += '{:>5}'.format(str(item))
+    # checks for empty set
     if len(set) == 0: temp += '\n\t\tEmpty Set'
     temp += '\n'
     return temp
 
-# runs test statement
-def test():
-    #card, limit = 5, 10
-    card, limit = 100, 200
-    sets = genSets(card, limit)
-    printResult(sets)
-    return
-
-# prints help menu
-def help():
-    print('Available Commands')
-    print('-------------------------------------------')
-    print('help - prints this list')
-    print('cls  - clears console screen')
-    print('test - test on assignment requirements')
-    print('eval - enters set input mode')
-    print('       enter 5 and 10 for an easy read test')
-    print('quit - exits program')
-
-# clears screen
-def clear(): print('\n' * 70)
-
-if __name__ == "__main__":
-    greeting('Sets v1')
+def main():
+    greeting('Sets v2')
     prompt()
     print('Goodbye')
+
+# program entry
+main()
